df0211816a7e6aa2edbfaaac7796b6704eb03fd9
John <johnschmidt241@gmail.com>
2021-07-27 21:35:26 -0700

    Adds functionality to read entered events to repeat and generates the dates necessary to enter into the database

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { Client } from "@notionhq/client"
 const notion = new Client({ auth: process.env.NOTION_KEY })
 
 const databaseId = process.env.NOTION_DATABASE_ID
+const databaseId2 = process.env.NOTION_DATABASE_ID_2;
 
 async function addItem(text) {
   try {
@@ -30,16 +31,71 @@ async function addItem(text) {
   }
 }
 
-addItem("Yurts in Big Sur, California")
+/**
 addItem("Test")
 let i = 0
 while (i < 3) {
   addItem("Test number " + i)
   i++
 }
-
+*/
+/**
 (async () => {
   const databaseId2 = process.env.NOTION_DATABASE_ID_2;
-  const response = await notion.databases.retrieve({ database_id: databaseId2 });
+  const response = await notion.databases.query({ database_id: databaseId2 });
   console.log(response);
+  
 })();
+*/
+
+const getDatabase = async () => {
+  const parameter = {
+    path: `databases/${databaseId2}/query`,
+    method: 'POST',
+  }
+  const { results } = await notion.request(parameter)
+
+  const data = results.map((page) => {
+    return {
+      id: page.id,
+      title: page.properties.Name.title[0]?.plain_text,
+      date: page.properties.Date.date.start,
+      endDate: page.properties.Date.date.end,
+      status: page.properties.Status.checkbox,
+      tags: page.properties.Tags.multi_select[0].name,
+      frequency: page.properties.Frequency.number,
+    }
+  })
+  return data;
+}
+
+const postDatabase = async () => {
+  const parameter = {
+    path: `databases/${databaseId}/request`
+  }
+}
+
+(async () => {
+  const notionData = await getDatabase()
+  for (let i = 0; i < notionData.length; i++) {
+    if (notionData[i].status == true) {
+
+      const date = new Date(parseInt(notionData[i].date.substr(0,4)),
+        parseInt(notionData[i].date.substr(5,2)) - 1,
+        parseInt(notionData[i].date.substr(8,2)))
+
+      const endDate = new Date(parseInt(notionData[i].endDate.substr(0,4)), 
+        parseInt(notionData[i].endDate.substr(5,2)) - 1,
+        parseInt(notionData[i].endDate.substr(8,2)))
+
+      const dates = [date]
+      const freq = notionData[i].frequency
+      var currentDate = date.getTime()
+      while (dates[dates.length - 1].getTime() <= endDate.getTime() - (freq * 86400000)){
+        dates.push(new Date(dates[dates.length - 1].getTime() + (freq * 86400000)))
+      }
+      console.log(dates)
+      
+    }
+  }
+})()
